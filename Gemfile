8b6715e5a36a6be60976ca9a84a089ca4340fcb8
RGu0000 <ryszardgurzeda@gmail.com>
2018-04-29 20:00:30 +0200

    Checkpoint before installing bootstrap theme

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'jquery-rails'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
@@ -24,6 +27,10 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'therubyracer'
+gem 'less-rails-bootstrap'
+gem "font-awesome-rails"
+gem 'jquery-easing-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,36 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.author_id = 2
+    @article.update_attributes(article_params)
+    if @article.save
+      flash[:notice] = 'You have added a new article.'
+      redirect_to articles_path
+    else
+      flash[:danger] = 'Failed to add new article.'
+    end
+  end
+
   def index
     @articles = Article.all
   end
 
+  def destroy
+    @article = Article.find(params[:id]).destroy
+    redirect_to articles_path
+  end
+
   def show
-    @article = Article.find(params[id])
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:body, :title)
   end
 end
