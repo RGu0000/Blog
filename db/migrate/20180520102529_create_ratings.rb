669fcbd30ccfef67a528a6ea89a06ce0fc2a41c3
RGu0000 <ryszardgurzeda@gmail.com>
2018-05-20 12:28:47 +0200

    Create migration for ratings

---
+++ db/migrate/20180520102529_create_ratings.rb
@@ -0,0 +1,10 @@
+class CreateRatings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ratings do |t|
+      t.decimal :rating, precision: 3, scale: 1
+      t.integer :author_id, null: false, index: true, foreign_key: true
+      t.integer :article_id, null: false, index: true, foreign_key: true
+      t.timestamps
+    end
+  end
+end
