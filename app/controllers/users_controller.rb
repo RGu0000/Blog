968651a8295e151905b102a52ab28d42fee5ae26
RGu0000 <ryszardgurzeda@gmail.com>
2018-05-23 21:55:41 +0200

    add bookmarks and rates to user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]).decorate
     @bookmarked_articles = Bookmark.includes(:article).where(user_id: @user.id)
+    @user_rates = Rating.includes(:article).where(author_id: @user.id)
   end
 end
