97d9348690e7d0982ac43efc2ec5b18f39750430
RGu0000 <ryszardgurzeda@gmail.com>
2018-05-31 23:53:51 +0200

    Setup omniauth facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable,
-        :omniauthable, omniauth_providers: [:google_oauth2]
+        :omniauthable, omniauth_providers: %i[google_oauth2 facebook]
 
   has_many :articles, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
@@ -16,12 +16,15 @@ class User < ApplicationRecord
 
     unless user
       password = Devise.friendly_token[0, 20]
-      user = User.create(
+      user = User.new(
         name: data['name'],
         email: data['email'],
         password: password,
-        password_confirmation: password
+        password_confirmation: password,
+        remote_avatar_url: data['image']
       )
+      user.email ||= "#{user.name.gsub(/\s+/, '')}@facebook.com"
+      user.save
     end
     user
   end
