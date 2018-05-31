9e5e04d72f2d47830957e90d551517dad3b2774d
RGu0000 <ryszardgurzeda@gmail.com>
2018-06-01 01:56:14 +0200

    Another try to setup facebook omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def facebook
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = User.from_facebook(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,21 @@ class User < ApplicationRecord
     end
     user
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] &&  session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_facebook(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image = auth.info.avatar
+    end
+  end
 end
