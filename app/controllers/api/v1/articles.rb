2e8c87756843278a69aca571b0a71ed7e2773532
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2018-10-07 18:24:34 +0200

    Add post method to articles api

--- app/controllers/api/v1/articles.rb
+++ app/controllers/api/v1/articles.rb
@@ -11,11 +11,28 @@ module API
 
         desc 'Return a article'
         params do
-          requires :id, type: String, desc: 'ID of the
+          requires :id, type: Integer, desc: 'ID of the
             article'
         end
         get ':id', root: 'article' do
           Article.where(id: permitted_params[:id]).first!
+        end
+
+        desc 'Creates an article'
+        params do
+          requires :author_id, type: Integer, desc: 'Id of the author'
+          requires :title, type: String, desc: 'Title of the article'
+          requires :body, type: String, desc: 'Body of the article'
+          requires :tag_id, type: Integer, desc: 'Tag'
+        end
+        post do
+          article = Article.new(
+            author_id: params[:author_id],
+            title: params[:title],
+            body: params[:body],
+          )
+          article.tags << Tag.find(params[:tag_id])
+          article.save!
         end
       end
     end
