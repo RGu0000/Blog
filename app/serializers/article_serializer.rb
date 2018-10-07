13fbea71df9ca7619211e6e7f404500bbef29d83
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2018-10-07 18:24:42 +0200

    Update article serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,5 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :body, :author_id, :created_at, :updated_at
 
   has_many :tags, serializer: TagSerializer
 end
